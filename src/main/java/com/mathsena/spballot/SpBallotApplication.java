ec2528b598daacf140d8bd21341fbe76e30f5034
MathSena <MathSena07@hotmail.com>
2024-09-11 20:11:49 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/spballot/SpBallotApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.spballot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBallotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpBallotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathsena/spballot/SpBallotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.spballot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBallotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
